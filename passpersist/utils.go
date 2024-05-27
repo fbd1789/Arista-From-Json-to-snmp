b39dcea308cc07f5ff39975e4573f109a5977d23
Jesse Mather <jmather@arista.com>
2024-05-27 19:38:59 +0000

    unused, leaving for reference

--- passpersist/utils.go
+++ passpersist/utils.go
@@ -1,14 +1,9 @@
 package passpersist
 
-import (
-	"fmt"
-	"strings"
-)
-
-func toHexStr(a []byte, sep string) string {
-	s := make([]string, len(a))
-	for i, b := range a {
-		s[i] = fmt.Sprintf("%02x", b)
-	}
-	return strings.Join(s, sep)
-}
+// func toHexStr(a []byte, sep string) string {
+// 	s := make([]string, len(a))
+// 	for i, b := range a {
+// 		s[i] = fmt.Sprintf("%02x", b)
+// 	}
+// 	return strings.Join(s, sep)
+// }
