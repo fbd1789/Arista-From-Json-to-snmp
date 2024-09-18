d09622d5b9da74bec994626b4229f36ceb93c52a
Jesse Mather <jmather@arista.com>
2024-09-18 20:58:08 +0000

    rename getbaseoid

--- utils/arista/utils.go
+++ utils/arista/utils.go
@@ -8,12 +8,12 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
-	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/arista-northwest/go-passpersist/passpersist"
+	"github.com/arista-northwest/go-passpersist/utils"
 	"github.com/go-cmd/cmd"
 )
 
@@ -40,7 +40,7 @@ func EosCommandJson(command string, v any) error {
 		buf.WriteString(l)
 	}
 
-	return json.Unmarshal(buf.Bytes(), v)
+	return json.Unmarshal(buf.Bytes(), &v)
 }
 
 func MustGetIfIndexeMap() map[string]int {
@@ -78,32 +78,34 @@ func GetIfIndexeMap() (map[string]int, error) {
 }
 
 // little hack to read the OID from the snmpd.conf file.  Can't think of a better way to do this yet
-func GetBaseOid() (passpersist.Oid, error) {
-	progName := filepath.Base(os.Args[0])
+func GetBaseOidFromSnmpConfig() (*passpersist.Oid, error) {
+	//progName := filepath.Base(os.Args[0])
 
 	file, err := os.Open("/etc/snmp/snmpd.conf")
 	//file, err := os.Open("snmpd.conf")
 	if err != nil {
-		return passpersist.Oid{}, errors.New("failed to read snmpd configuration file")
+		return nil, errors.New("failed to read snmpd configuration file")
 	}
 	defer file.Close()
 
-	re := regexp.MustCompile(`pass_persist (\.?[\.\d+]+) [\/\w+]*` + regexp.QuoteMeta(progName))
+	re := regexp.MustCompile(`pass_persist (\.?[\.\d+]+) [\/\w+]*` + regexp.QuoteMeta(utils.ProgName()))
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		m := re.FindStringSubmatch(scanner.Text())
 		if len(m) > 0 {
-			return passpersist.MustNewOid(m[1]), nil
+			o := passpersist.MustNewOid(m[1])
+			return &o, nil
 		}
 
 	}
-	return passpersist.Oid{}, errors.New("extension not found in configutation")
+	return nil, errors.New("extension not found in configutation")
 }
 
-func MustGetBaseOid() passpersist.Oid {
-	o, err := GetBaseOid()
+func MustGetBaseOid() *passpersist.Oid {
+	o, err := GetBaseOidFromSnmpConfig()
 	if err != nil {
 		slog.Error("failed to get base OID", slog.Any("error", err))
+		os.Exit(1)
 	}
 
 	return o
