522bed445119ee0d4c635599191f9fe3e66f9d81
Jesse Mather <jmather@arista.com>
2024-10-09 16:07:30 +0000

    add fields, fix typos

--- cmd/bgp_summary_vrf_all/main.go
+++ cmd/bgp_summary_vrf_all/main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"log/slog"
 	"log/syslog"
 	"net/netip"
-	"os"
-	"path/filepath"
-	"syscall"
 	"time"
 
 	"github.com/arista-northwest/go-passpersist/passpersist"
@@ -62,33 +57,36 @@ type ShowBGPSummaryVRFAll struct {
 }
 
 type VRF struct {
-	Name     string
+	Name     string `json:"vrf"`
 	RouterId IPAddress
 	Asn      int32 `json:",string"`
 	Peers    map[string]Peer
 }
 
 type Peer struct {
-	PrefixesReceived int
-	PrefixedAccepted int
-	UpDownTime       float64
-	State            string
+	PrefixReceived int
+	PrefixAccepted int
+	UpDownTime     float64
+	State          string
 }
 
 func init() {
 	logger.EnableSyslogger(syslog.LOG_LOCAL4, slog.LevelInfo)
-
-	f, _ := os.Create(fmt.Sprintf("/var/core/%s", filepath.Base(os.Args[0])))
-	//defer f.Close()
-	redirectStderr(f)
+	// redirectStderr(filepath.Base(os.Args[0]) + ".err")
 }
 
-func redirectStderr(f *os.File) {
-	err := syscall.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
-	if err != nil {
-		log.Fatalf("Failed to redirect stderr to file: %v", err)
-	}
-}
+// func redirectStderr(path string) {
+// 	f, err := os.Create(path)
+// 	if err != nil {
+// 		slog.Error("failed to create file", "path", path, slog.Any("err", err))
+// 		os.Exit(1)
+// 	}
+// 	err = syscall.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
+// 	if err != nil {
+// 		slog.Error("Failed to redirect stderr to file: %v", slog.Any("err", err))
+// 		os.Exit(1)
+// 	}
+// }
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -114,18 +112,19 @@ func main() {
 			slog.Error("failed to read data", slog.Any("error", err))
 			return
 		}
-		for name, vrf := range v4Data.VRFs {
+		for _, vrf := range v4Data.VRFs {
 			for s, peer := range vrf.Peers {
 				addr := IPAddress{netip.MustParseAddr(s)}
 				idx := addr.split()
-				idx = append(idx, utils.EncodeString(name)...)
+				idx = append(idx, utils.EncodeString(vrf.Name)...)
 
 				dur := time.Since(time.Unix(int64(peer.UpDownTime), 0))
-
-				pp.AddString(append([]int{1, 1, 1, 1}, idx...), addr.Addr.String())
-				pp.AddTimeTicks(append([]int{1, 1, 1, 2}, idx...), dur)
-				pp.AddInt(append([]int{1, 1, 1, 3}, idx...), int32(peer.PrefixedAccepted))
-				pp.AddInt(append([]int{1, 1, 1, 4}, idx...), int32(peer.PrefixesReceived))
+				pp.AddString(append([]int{1, 1, 1, 1}, idx...), vrf.Name)
+				pp.AddString(append([]int{1, 1, 1, 2}, idx...), addr.Addr.String())
+				pp.AddInt(append([]int{1, 1, 1, 3}, idx...), vrf.Asn)
+				pp.AddTimeTicks(append([]int{1, 1, 1, 4}, idx...), dur)
+				pp.AddInt(append([]int{1, 1, 1, 5}, idx...), int32(peer.PrefixReceived))
+				pp.AddInt(append([]int{1, 1, 1, 6}, idx...), int32(peer.PrefixAccepted))
 			}
 		}
 
@@ -135,18 +134,20 @@ func main() {
 			slog.Error("failed to read data", slog.Any("error", err))
 			return
 		}
-		for name, vrf := range v6Data.VRFs {
+		for _, vrf := range v6Data.VRFs {
 			for s, peer := range vrf.Peers {
 				addr := IPAddress{netip.MustParseAddr(s)}
 				idx := addr.split()
-				idx = append(idx, utils.EncodeString(name)...)
+				idx = append(idx, utils.EncodeString(vrf.Name)...)
 
 				dur := time.Since(time.Unix(int64(peer.UpDownTime), 0))
 
-				pp.AddString(append([]int{1, 2, 1, 1}, idx...), addr.Addr.String())
-				pp.AddTimeTicks(append([]int{1, 2, 1, 2}, idx...), dur)
-				pp.AddInt(append([]int{1, 2, 1, 3}, idx...), int32(peer.PrefixedAccepted))
-				pp.AddInt(append([]int{1, 2, 1, 4}, idx...), int32(peer.PrefixesReceived))
+				pp.AddString(append([]int{1, 2, 1, 1}, idx...), vrf.Name)
+				pp.AddString(append([]int{1, 2, 1, 2}, idx...), addr.Addr.String())
+				pp.AddInt(append([]int{1, 2, 1, 3}, idx...), vrf.Asn)
+				pp.AddTimeTicks(append([]int{1, 2, 1, 4}, idx...), dur)
+				pp.AddInt(append([]int{1, 1, 1, 5}, idx...), int32(peer.PrefixReceived))
+				pp.AddInt(append([]int{1, 1, 1, 6}, idx...), int32(peer.PrefixAccepted))
 			}
 		}
 	})
