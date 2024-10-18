3fd31cdfe7cd02e04c8e67f0195b39c770127f9e
fbd1789 <fabien@familledelmotte.eu>
2024-10-18 11:35:51 +0200

    Create showIpDhcpRelayCounters directory

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -19,13 +19,6 @@ func init() {
 	//logger.EnableSyslogger(syslog.LOG_LOCAL4, slog.LevelInfo)
 }
 
-// func redirectStderr(f *os.File) {
-// 	err := syscall.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
-// 	if err != nil {
-// 		log.Fatalf("Failed to redirect stderr to file: %v", err)
-// 	}
-// }
-
 func main() {
 	defer utils.CapPanic()
 
